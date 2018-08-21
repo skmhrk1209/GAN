bbd2fa37add71e83d7b0052838e21ea986f7e550
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-08-21 18:56:18 +0900

    update

--- dcgan.py
+++ dcgan.py
@@ -273,18 +273,18 @@ concat_logits = tf.concat([fake_logits, real_logits], axis=0)
 
 generator_eval_metric_op = tf.metrics.accuracy(
     labels=fake_labels,
-    predictions=tf.map_fn(
-        fn=lambda x: tf.cond(tf.cast(x, tf.float32) > 0.5, lambda: 1, lambda: 0),
+    predictions=tf.flatten(tf.map_fn(
+        fn=lambda x: tf.cond(x > 0.5, lambda: 1, lambda: 0),
         elems=fake_logits
-    )
+    ))
 )
 
 discriminator_eval_metric_op = tf.metrics.accuracy(
     labels=concat_labels,
-    predictions=tf.map_fn(
-        fn=lambda x: tf.cond(tf.cast(x, tf.float32) > 0.5, lambda: 1, lambda: 0),
+    predictions=tf.flatten(tf.map_fn(
+        fn=lambda x: tf.cond(x > 0.5, lambda: 1, lambda: 0),
         elems=concat_logits
-    )
+    ))
 )
 
 generator_loss = tf.losses.sigmoid_cross_entropy(
