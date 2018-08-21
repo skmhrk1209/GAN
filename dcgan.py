c428bf14dcaaa2249cfd76bc6badb992d5541dd8
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-08-21 18:45:38 +0900

    update

--- dcgan.py
+++ dcgan.py
@@ -271,7 +271,10 @@ fake_logits = discriminator(fakes, training=training)
 real_logits = discriminator(reals, training=training, reuse=True)
 concat_logits = tf.concat([fake_logits, real_logits], axis=0)
 
-print(concat_logits.get_shape())
+print(tf.map_fn(
+        fn=lambda x: tf.cond(x > 0.5, lambda: 1, lambda: 0),
+        elems=concat_logits
+    ).get_shape())
 
 generator_eval_metric_op = tf.metrics.accuracy(
     labels=fake_labels,
