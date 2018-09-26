3adc50a753f394134add7e7c3282e9177e30b1ed
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-09-26 16:09:16 +0900

    update

--- dcgan.py
+++ dcgan.py
@@ -394,24 +394,6 @@ class Model(object):
 
                         start = time.time()
 
-                        reals, fakes = session.run(
-                            [self.reals, self.fakes],
-                            feed_dict={
-                                self.latents: latents,
-                                self.training: True
-                            }
-                        )
-
-                        images = np.concatenate([reals, fakes], axis=2)
-
-                        images = utils.scale(images, -1, 1, 0, 1)
-
-                        for image in images:
-
-                            cv2.imshow("image", cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
-
-                            cv2.waitKey(100)
-
             except tf.errors.OutOfRangeError:
 
                 print("training ended")
