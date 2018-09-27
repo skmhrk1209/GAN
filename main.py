eda34afd077904ae0857f009b0d47021fc19ca7e
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-09-27 15:11:10 +0900

    update

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ import dataset
 import utils
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--model_dir", type=str, default="celeba_dcgan_model", help="model directory")
-parser.add_argument("--batch_size", type=int, default=100, help="batch size")
+parser.add_argument("--model_dir", type=str, default="celeba_wgan_gp_model", help="model directory")
+parser.add_argument("--batch_size", type=int, default=10, help="batch size")
 parser.add_argument("--num_epochs", type=int, default=10, help="number of training epochs")
 parser.add_argument("--buffer_size", type=int, default=100000, help="buffer size to shuffle dataset")
 parser.add_argument('--data_format', type=str, choices=["channels_first", "channels_last"], default="channels_last", help="data_format")
