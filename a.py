2e707d33956565257677829d4b60403b674a92cb
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-09-28 13:21:23 +0900

    update

---
+++ a.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+import cv2
+
+mnist = tf.contrib.learn.datasets.load_dataset("mnist")
+
+for i, image in enumerate(mnist.train.images):
+
+    cv2.imwrite("data/train/mnist/{}.jpg".format(i), image.reshape([28, 28, 1]) * 255)
+
+for i, image in enumerate(mnist.test.images):
+
+    cv2.imwrite("data/test/mnist/{}.jpg".format(i), image.reshape([28, 28, 1]) * 255)
